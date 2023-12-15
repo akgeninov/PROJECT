eb8915a1e54ad5ad9da5cd8a46a8f34d3555fcd6
fildzahattira <fildzahattira560@gmail.com>
2023-12-15 09:44:56 +0700

    process fe

--- client/src/components/career-lowongan/section2/Section_2_lowongan.jsx
+++ client/src/components/career-lowongan/section2/Section_2_lowongan.jsx
@@ -15,25 +15,28 @@ function Section_2_lowongan(props) {
       onClick={handleContainerClick}
     >
       <div
-        className="grid grid-cols-2 divide-x shadow-inner rounded"
-        style={{ width: "1080px", height: "160px" }}
+        className="grid grid-cols-2 divide-x rounded border-none p-6" 
+        style={{
+          width: "1080px",
+          height: "160px",
+          boxShadow: "2px 2px 12px 0px #0101011A",
+        }}
       >
-        <div>
+        <div className="border-none"> {/* Menggunakan border-none pada elemen anak */}
           <div className="grid grid-cols-1 divide-y">
-            <div className="font-bold text-lg">{props.lowongan.nama_lowongan_pekerjaan}</div>
-            <div className="text-gray-500">{props.lowongan.departemen.nama_departemen_pekerjaan}</div>
-            <div>
-              <div className="grid grid-cols-3 divide-x">
-                <div className="flex justify-center">{props.lowongan.periode_pekerjaan.nama_periode_perkerjaan}</div>
-                <div className="flex justify-center">{props.lowongan.tipe_pekerjaan.nama_tipe__perkerjaan}</div>
-                <div className="flex justify-center">{props.lowongan.jenjang_pekerjaan.nama_jenjang__pekerjaan}</div>
+            <div className="font-bold text-[22px] border-none">{props.lowongan.nama_lowongan_pekerjaan}</div>
+            <div className="font-semibold text-[#12517C] text-[16px] border-none">{props.lowongan.departemen.nama_departemen_pekerjaan}</div>
+            <div className="border-none">
+              <div className="grid grid-cols-3 divide-x lg:mt-[30px] lg:text-[16px]">
+                <div className="flex justify-center border-none">{props.lowongan.periode_pekerjaan.nama_periode_perkerjaan}</div>
+                <div className="flex justify-center border-none">{props.lowongan.tipe_pekerjaan.nama_tipe__perkerjaan}</div>
+                <div className="flex justify-center border-none">{props.lowongan.jenjang_pekerjaan.nama_jenjang__pekerjaan}</div>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-center items-end h-full"> {/* Menggunakan flex-col dan items-end untuk menurunkan ke tengah bawah */}
-          <div>Batas Lamar :&nbsp;&nbsp;<br/>{props.lowongan.batas_lamar}&nbsp;&nbsp;
-           </div>
+        <div className="flex flex-col justify-center items-end h-full border-none"> {/* Menggunakan border-none pada elemen anak */}
+          <div>Batas Lamar :&nbsp;&nbsp;<br/>{props.lowongan.batas_lamar}&nbsp;&nbsp;</div>
         </div>
       </div>
     </div>
