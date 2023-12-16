95feecb9c56f028a773b4a46ce8b0ad86d416783
lukman-lucc <mannnb7@gmail.com>
2023-12-16 20:31:47 +0700

    dashboard profile

--- client/src/components/profile/dashboard/dashboardProfile.jsx
+++ client/src/components/profile/dashboard/dashboardProfile.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 function DashboardProfile () {
 return (
-        <div className=" flex flex-col justify-start lg:mt-[10px] lg:mb-[60px] px-[10px] lg:px-0 w-full lg:w-[100%] h-full lg:h-[444px] gap-[52px]"> 
-        <div className="flex felx-col justify-start">
-        <ul>
-          <li>Dashboard</li>
-          <li>Kelas Saya</li>
-          <li>Wishlist</li>
-          <li>Event</li>
-          <li>Transaksi</li>
-        </ul>
+        <div className=" flex flex-col justify-start lg:mt-[10px]  px-[10px] lg:px-0 w-full lg:w-[100%] h-full lg:h-[650px] gap-[52px]"> 
+        <div
+        className="w-[300px] h-full bg-white-200 border border-grey ml-10px" style={{
+          boxShadow: "1px 1px 3px rgba(128, 128, 128, 0.3)" // Ganti warna menjadi abu-abu
+        }}
+      >
+      
+
+        
       </div>
     </div>
   );
