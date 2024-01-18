22f7a7514ec2dddc5319b740b68f68bac0b00752
Et-Shafa <naadiramalaika.atsiila@gmail.com>
2024-01-18 20:38:15 +0700

    add API transaction page user

--- server/routers/kelasTransaksiRouters.js
+++ server/routers/kelasTransaksiRouters.js
@@ -4,5 +4,8 @@ const { verifyToken } = require("../middleware/verifyToken");
 
 router.post("/addToKelasTransaksi", verifyToken, kelasTransaksiController.addToKelasTransaksi);
 router.get("/getTransaksiByIdUser", verifyToken, kelasTransaksiController.getTransaksiByIdUser);
+router.get("/getTransaksiByIdUser/success", verifyToken, kelasTransaksiController.getTransaksiByIdUserSuccess);
+router.get("/getTransaksiByIdUser/pending", verifyToken, kelasTransaksiController.getTransaksiByIdUserPending);
+router.get("/getTransaksiByIdUser/canceled", verifyToken, kelasTransaksiController.getTransaksiByIdUserCanceled);
 
 module.exports = router;
