1a95c830e403a447750dcfed88d73bb7c5a5eb88
RosalenaBrigita <ocalimbong@gmail.com>
2024-01-21 00:01:24 +0700

    addBool-transaksi&wishlist

--- server/models/kelas_wishlist.js
+++ server/models/kelas_wishlist.js
@@ -1,6 +1,6 @@
 'use strict';
 const {
-  Model
+  Model, BOOLEAN
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class kelas_wishlist extends Model {
@@ -15,18 +15,24 @@ module.exports = (sequelize, DataTypes) => {
           name: "id_kelas_bisnis",
         }
       })
-      // kelas_wishlist.belongsTo(models.kelas_kategori,{
-      //   foreignKey: {
-      //     name: "id_kelas_kategori",
-      //   }
-      // })
     }
   }
-  kelas_wishlist.init({
-    date_wishlist: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'kelas_wishlist',
-  });
+  kelas_wishlist.init(
+    {
+      date_wishlist: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      isRemove: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'kelas_wishlist',
+    }
+  );
   return kelas_wishlist;
 };
--- server/routers/kelasTransaksiRouters.js
+++ server/routers/kelasTransaksiRouters.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const kelasTransaksiController = require("../controllers/kelasTransaksiController");
 const { verifyToken } = require("../middleware/verifyToken");
 
-router.post("/addToKelasTransaksi", verifyToken, kelasTransaksiController.addToKelasTransaksi);
+router.post("/changeTransaksiBool", verifyToken, kelasTransaksiController.changeTransaksiBool);
+router.put("/updateStatusTransaksi", verifyToken, kelasTransaksiController.updateStatusTransaksi);
 router.get("/getTransaksiByIdUser", verifyToken, kelasTransaksiController.getTransaksiByIdUser);
 
 module.exports = router;
